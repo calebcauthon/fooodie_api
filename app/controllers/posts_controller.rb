fc5a04d42b7420d28e1ebf24888a3cc452ee1b2a
Caleb Cauthon <calebcauthon@gmail.com>
2015-11-19 16:40:47 +1300

    Allow editing and enable CORS requests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  skip_before_filter :verify_authenticity_token, :only => [:create]
+  skip_before_filter :verify_authenticity_token, :only => [:update, :create]
+
+  def options
+    render :json => []
+  end
 
   # GET /posts
   # GET /posts.json
@@ -47,25 +51,29 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    @post.update(post_params)
+    render :json => @post
+
+    #respond_to do |format|
+    #  if @post.update(post_params)
+    #    format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+    #    format.json { render :show, status: :ok, location: @post }
+    ##  else
+     #   format.html { render :edit }
+     #   format.json { render json: @post.errors, status: :unprocessable_entity }
+     # end
+    #end
   end
 
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    render :json => []
+    #respond_to do |format|
+    #  format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+    #  format.json { head :no_content }
+    #end
   end
 
   private
