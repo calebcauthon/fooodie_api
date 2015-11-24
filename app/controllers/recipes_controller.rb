57d943cd9e86708e80b533a9e078c26193a606d1
Caleb Cauthon <calebcauthon@gmail.com>
2015-11-25 10:23:16 +1300

    Delete and edit recipes.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,9 @@
 class RecipesController < ApplicationController
   respond_to :html, :json
+  skip_before_filter :verify_authenticity_token
+
+  before_action :authenticate_user!, only: [:update, :destroy]
+  before_action :set_recipe, only: [:update, :destroy]
 
   def index
     @recipes = Recipe.all
@@ -16,8 +20,22 @@ class RecipesController < ApplicationController
   def show
     render :json => Recipe.find(params[:id])
   end
-  
+
+  def update
+    @recipe.update(recipe_params)
+    render :json => @recipe
+  end
+
+  def destroy
+    @recipe.destroy
+    render :json => @recipe
+  end
+
   private
+    
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
 
     def recipe_params
       params.require(:recipe).permit(:title, :body)
