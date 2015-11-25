06c75188e00dece1accc97347acdc692a1133b55
Caleb Cauthon <calebcauthon@gmail.com>
2015-11-25 17:18:32 +1300

    Remove the CORS stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,6 @@ Rails.application.routes.draw do
   resources :recipes
   resources :users
   
-  namespace :api do
-    get :csrf, to: 'csrf#index'
-  end
-
   root :to => "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
